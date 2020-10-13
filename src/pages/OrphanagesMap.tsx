af34eaf8bfa69e24297f2bf8f0e439e4f7bdacf0
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-10-13 20:32:17 -0300

    :pencil: update(frontend): create inital orphanagesmap page structure

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { FiPlus } from 'react-icons/fi';
+import mapMarkerImg from '../images/map-marker.svg'
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
   return (
-    <h1>Hello World</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img alt="Happy" src={mapMarkerImg} />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+          <footer>
+            <strong>São Paulo</strong>
+            <span>Taboão da Serra</span>
+          </footer>
+        
+      </aside>
+
+      <div></div>
+
+      <Link className="create-orphanage" to="">
+        <FiPlus color="#FFF" size={32} />
+      </Link>
+    </div>
   );
 }
 
