4cb6ccb7a00582f7438f17e5ce2037effc9d13f6
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-10-14 08:56:19 -0300

    :pencil: update(frontend): add new pages
    - CreateOrphanage page;
    - Orphanage page.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import CreateOrphanage from './pages/CreateOrphanage';
 import Landing from './pages/Landing';
+import Orphanege from './pages/Orphanage';
 import OrphanegesMap from './pages/OrphanagesMap';
 
 function Routes() {
-  return(
+  return (
     <BrowserRouter>
       <Switch>
         <Route component={Landing} exact path="/" />
         <Route component={OrphanegesMap} path="/app" />
+        <Route component={CreateOrphanage} path="/orphanages/create" />
+        <Route component={Orphanege} path="/orphanages/:id" />
       </Switch>
     </BrowserRouter>
   );
